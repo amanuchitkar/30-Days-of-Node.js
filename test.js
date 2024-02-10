07dda7f51bf470ce1bc92970ec025d2d45f5988b
aman uchitkar <amanuchitkar@gmail.com>
2024-02-10 12:58:08 +0530

    2 day on Node.js

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-console.log("Hello")
+const calculator=require('./calculator')
+// console.log("Hello")
+
+// function sayhellow() {
+//     console.log("hello")
+// }
+// sayhellow()
+
+// console.log(window)
+// console.log(global)
+// let age=30
+// console.log(global.age)
+calculator.addition(5,7)
+calculator.substaction(5,7)
+calculator.multiplucation(5,7)
+calculator.division(5,7)
