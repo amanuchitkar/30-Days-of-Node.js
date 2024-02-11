c4e7436fe6f512835326c1a8c9ce203afc89c677
aman uchitkar <amanuchitkar@gmail.com>
2024-02-11 19:37:27 +0530

    30 Days on Node.js

---
+++ Node_Moudule_system/3_path.js
@@ -0,0 +1,8 @@
+const path=require("path")
+
+let ext=path.extname("f1.txt")
+let baseName=path.basename("C:\\Users\\Aman\\OneDrive\\Documents\My Code\\30 Days of Node js\\Node_Moudule_system\\f1.txt")
+console.log(ext)
+console.log(baseName)
+console.log(__filename)
+console.log(__dirname)
