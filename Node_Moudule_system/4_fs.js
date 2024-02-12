94ca49da59e6a578d1bb1c389bce55338a22d5ee
aman uchitkar <amanuchitkar@gmail.com>
2024-02-12 15:36:47 +0530

    30 Days of Node.js

--- Node_Moudule_system/4_fs.js
+++ Node_Moudule_system/4_fs.js
@@ -3,8 +3,15 @@ const fs=require("fs")
 
 
 // read file
-let fileContent=fs.readFileSync('./f1.txt')
-console.log("data of the fiel =>" + fileContent)
-//Write in side the file
+// let fileContent=fs.readFileSync('./f1.txt')
+// console.log("data of the fiel =>" + fileContent)
+// //Write in side the file
 
-fs.writeFileSync("f2.txt","This is file 2")
+// fs.writeFileSync("f2.txt","This is file 2")
+// console.log("file has been writen")
+
+// fs.appendFileSync("f3.txt"," Data update succesfully")
+// console.log("file has been append Data")
+
+fs.unlinkSync("f2.txt")
+console.log("file has been Deleted")
