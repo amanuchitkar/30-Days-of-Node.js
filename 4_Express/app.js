69000ce83322505612bfa83973c92c135ebb75e0
aman uchitkar <amanuchitkar@gmail.com>
2024-02-16 23:27:55 +0530

    30 Days of Node.js

--- 4_Express/app.js
+++ 4_Express/app.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const courses=[
+  {id:1,name:"javascript"},
+  {id:2,name:"java"},
+  {id:3,name:"python"},
+  {id:4,name:"c++"},
+]
 
 app.get('/', (req, res)=> {
   res.send('Hello World')
@@ -9,6 +15,17 @@ app.get('/about',(req,res)=>{
 })
 app.get('/contact',(req,res)=>{
   res.send("You are in contact page")
+})
+
+// Route parameters
+app.get("/courses/:coursename",(req,res)=>{
+  // let course=courses.find(course=>course.id===parseInt(req.params.id))
+  let course=courses.find(course => course.name===req.params.coursename)
+  console.log(course)
+  // res.send(req.params.id)
+  // res.send(req.params)
+  if (!course) res.status(404).send("This is not found")
+  res.send(course)
 })
 const port=process.env.PORT || 3000
 app.listen(port,()=>console.log(`Port is runing on ${port}`))
