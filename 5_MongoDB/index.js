1b5bb81d113a0aba60246f4bfd31cce7f753aedb
aman uchitkar <amanuchitkar@gmail.com>
2024-02-23 12:16:20 +0530

    30 Days of Node.js

--- 5_MongoDB/index.js
+++ 5_MongoDB/index.js
@@ -5,3 +5,24 @@ mongoose.connect('mongodb://localhost/testDatabase')
 .catch(err=>console.log("could'n connect mongodb",err))
 
 //schema
+const courseschema=new mongoose.Schema({
+    name:String,
+    creator:String,
+    publishedDate:{type:Date,default:Date.now},
+    isPublished:Boolean
+}) 
+
+const Course=mongoose.model('Course',courseschema)
+
+async function creatcourse() {
+    
+    const course=new Course({
+        name:'Javascript',
+        creator:'mrinal',
+        isPublished:true
+    })
+    const result=await course.save()
+    console.log(result)
+}
+creatcourse()
+    
