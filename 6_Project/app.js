cedd713571bd0d828504edd4437f76ff7c0ae9ea
aman uchitkar <amanuchitkar@gmail.com>
2024-02-28 12:34:50 +0530

    30 Days of Node.js

--- 6_Project/app.js
+++ 6_Project/app.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const mongoose=require('mongoose')
 // const mymiddelware = require('./middel')
 const categories=require('./categories')
 const joi=require('Joi')
 const app = express()
 
+mongoose.connect('mongodb://localhost/LearningPlatform')
+.then(()=> console.log("Connect Succesfully"))
+.catch(err=>console.log('Coudnot connect to mongodb',err))
+
+
+app.use(express.json())
 app.use(categories)
 
 const port = process.env.PORT || 3000
--- 6_Project/categories.js
+++ 6_Project/categories.js
@@ -1,60 +1,66 @@
-const express=require('express');
+const express = require('express');
 const Joi = require('Joi');
+const mongoose = require('mongoose')
 
-const router=express.Router()
+const router = express.Router()
 
 router.use(express.json())
 // router.use(mymiddelware)
-const categories = [
-    { id: 1, name: 'web' },
-    { id: 2, name: 'mobile' },
-    { id: 3, name: 'Photography' }
-]
 
-router.get('/api/categories', (req, res) => {
+
+const categoriesschema = new mongoose.Schema({
+    name: { type: String, required: true, minlength: 4, maxlength: 30 },
+
+})
+const Category = mongoose.model('category', categoriesschema)
+
+
+
+
+router.get('/api/categories', async (req, res) => {
+    let categories = await Category.find()
     res.send(categories);
 });
 
-router.post('/api/categories', (req, res) => {
-    const {error}=validateData(req.body)
+router.post('/api/categories', async (req, res) => {
+    const { error } = validateData(req.body)
     if (error) res.status(400).send(error.details[0].message)
-    const category = {
-        id: categories.length + 1,
+    const category = new Category({
         name: req.body.name
-    };
-    categories.push(category);
+    })
+    await category.save();
     res.send(category);
 });
 
-router.put("/api/categories/:id", (req, res) => {
-    const category = categories.find(c => c.id === parseInt(req.params.id));
-    if (!category) return res.status(404).send("The web page not found")
-    // if (error) return res.status(404).send(error.details[0].message);
-    // console.error(error.errors)
-    category.name = req.body.name
-    res.send(category)
-    
-});
+// router.put("/api/categories/:id", (req, res) => {
+//     const category = categories.find(c => c.id === parseInt(req.params.id));
+//     if (!category) return res.status(404).send("The web page not found")
+//     // if (error) return res.status(404).send(error.details[0].message);
+//     // console.error(error.errors)
+//     category.name = req.body.name
+//     res.send(category)
 
-router.delete('/api/categories/:id',(req,res)=>{
-    const category=categories.find(c => c.id === parseInt(req.params.id));
-    if (!category) return res.status(404).send("The web page not found");
-    const index=categories.indexOf(category);
-    categories.splice(index,1);
-    res.send(category);
-    
-});
+// });
 
-router.get('/api/categories/:id', (req, res) => {
-    const category=categories.find(c => c.id === parseInt(req.params.id));
-    if (!category) return res.status(404).send("The web page not found");
-    res.send(category);
-});
+// router.delete('/api/categories/:id', (req, res) => {
+//     const category = categories.find(c => c.id === parseInt(req.params.id));
+//     if (!category) return res.status(404).send("The web page not found");
+//     const index = categories.indexOf(category);
+//     categories.splice(index, 1);
+//     res.send(category);
+
+// });
+
+// router.get('/api/categories/:id', (req, res) => {
+//     const category = categories.find(c => c.id === parseInt(req.params.id));
+//     if (!category) return res.status(404).send("The web page not found");
+//     res.send(category);
+// });
 
 function validateData(category) {
-    const schema={
-        name:Joi.string().min(3).required()
+    const schema = {
+        name: Joi.string().min(3).required()
     }
-    return Joi.validate(category,schema)
+    return Joi.validate(category, schema)
 }
-module.exports=router
+module.exports = router
