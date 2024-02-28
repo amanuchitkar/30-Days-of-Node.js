b4915f185ea21f15b8e0f53ac47bbd06eb896b1c
aman uchitkar <amanuchitkar@gmail.com>
2024-02-28 12:35:30 +0530

    30 Days of Node.js

--- 6_Project/categories.js
+++ 6_Project/categories.js
@@ -26,36 +26,40 @@ router.post('/api/categories', async (req, res) => {
     const { error } = validateData(req.body)
     if (error) res.status(400).send(error.details[0].message)
     const category = new Category({
-        name: req.body.name
-    })
+name: req.body.name
+})
     await category.save();
     res.send(category);
 });
 
-// router.put("/api/categories/:id", (req, res) => {
-//     const category = categories.find(c => c.id === parseInt(req.params.id));
-//     if (!category) return res.status(404).send("The web page not found")
-//     // if (error) return res.status(404).send(error.details[0].message);
-//     // console.error(error.errors)
-//     category.name = req.body.name
-//     res.send(category)
+router.put("/api/categories/:id", async (req, res) => {
+    const {error}=validateData(req.body)
+    if (error) res.status(400).send(error.details[0].message)
+    const category= await Category.findByIdAndUpdate(req.params.id,{name:req.body.name},{new:true})
+    // const category = categories.find(c => c.id === parseInt(req.params.id));
+    if (!category) return res.status(404).send("The web page not found")
+    // if (error) return res.status(404).send(error.details[0].message);
+    // console.error(error.errors)
+    // category.name = req.body.name
+    res.send(category)
 
-// });
+});
 
-// router.delete('/api/categories/:id', (req, res) => {
-//     const category = categories.find(c => c.id === parseInt(req.params.id));
-//     if (!category) return res.status(404).send("The web page not found");
-//     const index = categories.indexOf(category);
-//     categories.splice(index, 1);
-//     res.send(category);
+router.delete('/api/categories/:id', async (req, res) => {
+    const category = await Category.findByIdAndDelete(req.params.id)
+    if (!category) return res.status(404).send("The web page not found");
+    // const index = categories.indexOf(category);
+    // categories.splice(index, 1);
+    res.send(category);
 
-// });
+});
 
-// router.get('/api/categories/:id', (req, res) => {
-//     const category = categories.find(c => c.id === parseInt(req.params.id));
-//     if (!category) return res.status(404).send("The web page not found");
-//     res.send(category);
-// });
+router.get('/api/categories/:id', async (req, res) => {
+    const category = await Category.findById(req.params.id)
+    // const category = categories.find(c => c.id === parseInt(req.params.id));
+    if (!category) return res.status(404).send("The web page not found");
+    res.send(category);
+});
 
 function validateData(category) {
     const schema = {
